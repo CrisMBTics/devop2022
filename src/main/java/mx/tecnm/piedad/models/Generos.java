33343682cbb24c26fd27514e0f5b364c9d6ce6a0
JuanG7u7 <juan14yure@gmail.com>
2022-03-15 15:07:06 -0600

    generos xd

--- src/main/java/mx/tecnm/piedad/models/Generos.java
+++ src/main/java/mx/tecnm/piedad/models/Generos.java
@@ -1,5 +1,23 @@
 package mx.tecnm.piedad.models;
 
 public class Generos {
+	
+	private int id;
+	private String nombre;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 
 }
